81cf4742ba0d9915e448bc8d32b01690416deadc
Bhishan Poudel <bhishanpdl@users.noreply.github.com>
2024-05-07 15:00:28 -0400

    Add files via upload

---
+++ module_tabula_parse_table_from_pdf/tabula_examples/readme.txt
@@ -0,0 +1,14 @@
+# Introduction to tabula
+References:
+- https://www.java.com/en/download/manual.jsp
+- Download 64bit offline binary and install the java
+
+# Enviroment Variables for JAVA
+```bash
+Win R > sysdm.cpl > Advanced 
+JAVA_HOME=C:\Program Files (x86)\Common Files\Oracle\Java
+```
+
+# Install jpype1 (j-pipe-one)
+get_env py311_camelot
+pip install jpype1
---
+++ module_tabula_parse_table_from_pdf/tabula_examples/test_tabula.py
@@ -0,0 +1,20 @@
+import sys
+print(sys.executable)
+
+import tabula
+print(f"{tabula.__version__=}")
+
+print('\nMake sure JAVA is installed and JAVA_HOME is set to use the module tabula.')
+import os
+print(os.getenv('JAVA_HOME'))
+print()
+
+path_pdf = r"C:\Users\a126291\OneDrive - AmerisourceBergen(ABC)\data\pdf_files\pdf_with_data_tables\non_scan_pdf_with_text_and_table_from_camelot.pdf"
+
+tables = tabula.read_pdf(path_pdf, pages="all", stream=True, silent=True)
+num_tables = len(tables)
+print(f"Found: {num_tables} tables (tabula gives pandas dataframes)")
+
+table = tables[0]
+print(f"{type(table)=}")
+print(table.head())
